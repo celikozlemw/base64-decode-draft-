5d3e0e50e7136f35b6361e26de590f04cc0d8374
ÖZLEM <celik.ozlem.05062003@gmail.com>
2023-06-30 17:16:25 +0300

    Update decoder.c

--- decoder.c
+++ decoder.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 int main() {
-    // Define the character set used for Base64 encoding.
+    // kullanılan karakter setini tanımlayın(hangi dil ise o karakterleri içermeli)
     const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    // Get the Base64-encoded string from the user.
+    
     char *encoded_string = malloc(10000);
     scanf(" %s ", encoded_string);
-    // Initialize the decoded string.
+    
     char *decoded_string = malloc(10000);
     int decoded_string_length = 0;
-    // Decode the Base64-encoded string.
+    //kod çözücü döngü
     for (int i = 0; i < strlen(encoded_string); i += 4) {
-        // Get the four Base64 characters at the current position in the encoded string.
+    
         char c1 = encoded_string[i];
         char c2 = encoded_string[i + 1];
         char c3 = encoded_string[i + 2];
         char c4 = encoded_string[i + 3];
-        // Convert the four Base64 characters into three bytes.
+    
         int b1 = base64_chars[c1] - 'A';
         int b2 = base64_chars[c2] - 'A';
         int b3 = base64_chars[c3] - 'A';
         int b4 = base64_chars[c4] - 'A';
-        // If the fourth Base64 character is '=', then we need to pad the decoded byte with 0.
+        // dördüncü Base64 karakteri '=' ise kodu çözülmüş baytı 0 yap
         if (c4 == '=') {
             b4 = 0;
         }
-        // Append the three decoded bytes to the decoded string.
+        
         decoded_string[decoded_string_length++] = (b1 << 2) | (b2 >> 4);
         decoded_string[decoded_string_length++] = (b2 << 4) | (b3 >> 2);
         decoded_string[decoded_string_length++] = (b3 << 6) | b4;
